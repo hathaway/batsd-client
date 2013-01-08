ec0149f86c6b7933178386a66b86489005268f17
Ben Hathaway <ben.hathaway@gmail.com>
2013-01-08 16:56:32 -0500

    Add comments.

--- lib/batsd.rb
+++ lib/batsd.rb
@@ -2,8 +2,12 @@ require 'timeout'
 require 'json'
 require 'socket'
 
+##
+# A batsd client for querying data from a batsd server.
 class Batsd
 
+	##
+	# The default option values.
 	DEFAULTS = {
 		:host => "127.0.0.1",
 		:port => 8127,
@@ -11,18 +15,27 @@ class Batsd
 		:max_attempts => 2
 	}
 
+	##
+	# The hostname of the batsd server.
 	def host
       	@options[:host]
     end
 
+    ##
+    # The port the batsd server is running on.
     def port
       	@options[:port]
     end
 
+    ##
+    # The timeout in milliseconds to use for the TCP connection.
     def timeout
       	@options[:timeout]
     end
 
+    ##
+    # The maxiumum number of attempts to make when querying for data. 
+    # If an error is encountered, it will try this many total times before throwing an exception.
     def max_attempts
     	@options[:max_attempts]
     end
@@ -34,12 +47,17 @@ class Batsd
 		connect!
     end
 
+    ##
+    # Send the 'ping' command to the server.
+	#
+	# @return [String] The response received from the server.
     def ping
         send_command("ping")
     end
 
-    # Get the set of datapoints batsd knows about. Will return an array of
-    # strings
+    # Queries for the available keys in the batsd server.
+	#
+	# @return [Array] An array of string keys.
     def available
         keys = send_command("available")
         keys.collect do |k|
@@ -51,9 +69,25 @@ class Batsd
         end.flatten
     end
 
-    # Get the values for a given <code>metric_name</code> that's contained in the available
+    ##
+    # Get the stats for a given <code>metric_name</code> that's contained in the available
     # set of datapoints within the range of <code>start_timestamp</code> to
     # <code>end_timestamp</code>
+	#
+	# @param [String] metric_name
+	# 	The key for the metric you are querying for.
+	#
+	# @param [Time] start_timestamp
+	# 	The start of the time range of the query.
+	#
+	# @param [Time] end_timestamp
+	# 	The end of the time range of the query. Defaults to the current time.
+	#
+	# @param [Integer] attempts
+	# 	The number of attempts that have been made for this query. Defaults to 0.
+	#
+	# @return [Array] An array of hashes. Each hash contains the <code>:timestamp</code>
+	# 	and <code>:value</code> of the data point.
     def stats(metric_name, start_timestamp, end_timestamp=Time.now, attempt=0)
         results = []
         values = send_command("values #{metric_name} #{start_timestamp.to_i} #{end_timestamp.to_i}")
@@ -68,10 +102,40 @@ class Batsd
         results
     end
 
+    ##
+    # Get only the values of the stats for a given <code>metric_name</code> 
+    # that's contained in the available set of datapoints within the range 
+    # of <code>start_timestamp</code> to <code>end_timestamp</code>
+	#
+	# @param [String] metric_name
+	# 	The key for the metric you are querying for.
+	#
+	# @param [Time] start_timestamp
+	# 	The start of the time range of the query.
+	#
+	# @param [Time] end_timestamp
+	# 	The end of the time range of the query. Defaults to the current time.
+	#
+	# @return [Array] An array of floating point numbers.
     def values(metric_name, start_timestamp, end_timestamp=Time.now)
     	stats(metric_name, start_timestamp, end_timestamp).map{|s| s[:value]}
     end
 
+    ##
+    # Get only the timestamps of the stats for a given <code>metric_name</code> 
+    # that's contained in the available set of datapoints within the range 
+    # of <code>start_timestamp</code> to <code>end_timestamp</code>
+	#
+	# @param [String] metric_name
+	# 	The key for the metric you are querying for.
+	#
+	# @param [Time] start_timestamp
+	# 	The start of the time range of the query.
+	#
+	# @param [Time] end_timestamp
+	# 	The end of the time range of the query. Defaults to the current time.
+	#
+	# @return [Array] An array of Time objects.
     def timestamps(metric_name, start_timestamp, end_timestamp=Time.now)
     	stats(metric_name, start_timestamp, end_timestamp).map{|s| s[:timestamp]}
     end
@@ -79,6 +143,9 @@ class Batsd
 
     protected
 
+    ##
+    # Parse the options provided in the constructor. Use the defaults for
+    # any options not provided.
     def _parse_options(options)
 		defaults = DEFAULTS.dup
 		options = options.dup
@@ -92,7 +159,8 @@ class Batsd
 
     private
 
-    	# Connect to the remote batsd server over TCP
+    	##
+    	# Connect to the remote batsd server over TCP.
 		def connect!
 			Timeout::timeout(5) do
 				self.remote = TCPSocket.new(host, port)
@@ -103,16 +171,27 @@ class Batsd
 				raise CannotConnectError
 		end
 
+		##
+		# Disconnect from the remote batsd server.
 		def disconnect
 			self.remote.close
 		end
 
+		##
+		# Reconnect to the batsd server.
 		def reconnect!
 			disconnect
 			connect!
 		end
 
+		##
 		# Send a command to the remote and attempt to parse the response as JSON
+		#
+		# @param [String] command
+		# 	The command to send to the server.
+		#
+		# @return [Array, String] The response received from the server. Unless it is 
+		# 	the ping command, the JSON response is parsed into an array.
         def send_command(command, attempt=0)
             Timeout::timeout(timeout.to_f / 1000.0) do
                 connect! unless self.remote
--- lib/batsd/errors.rb
+++ lib/batsd/errors.rb
@@ -1,33 +1,41 @@
 class Batsd
-  # Base error for all batsd errors.
-  class BaseError < RuntimeError
-  end
+    ##
+    # Base error for all batsd errors.
+    class BaseError < RuntimeError
+    end
 
-  # Base error for connection related errors.
-  class BaseConnectionError < BaseError
-  end
+    ##
+    # Base error for connection related errors.
+    class BaseConnectionError < BaseError
+    end
 
-  # Raised when connection to a batsd server cannot be made.
-  class CannotConnectError < BaseConnectionError
-  end
+    ##
+    # Raised when connection to a batsd server cannot be made.
+    class CannotConnectError < BaseConnectionError
+    end
 
-  # Raised when a new connection times out
-  class ConnectionTimeoutError < BaseConnectionError
-  end
+    ##
+    # Raised when a new connection times out
+    class ConnectionTimeoutError < BaseConnectionError
+    end
 
-  # Base error for command related errors.
-  class BaseCommandError < BaseError
-  end
+    ##
+    # Base error for command related errors.
+    class BaseCommandError < BaseError
+    end
 
-  # Raised when a command could not be run
-  class CommandError < BaseCommandError
-  end
+    ##
+    # Raised when a command could not be run
+    class CommandError < BaseCommandError
+    end
 
-  # Raised when a command times out
-  class CommandTimeoutError < BaseCommandError
-  end
+    ##
+    # Raised when a command times out
+    class CommandTimeoutError < BaseCommandError
+    end
 
-  # Raised when the values returned weren't the same as what was asked for
-  class InvalidValuesError < BaseError
-  end
+    ##
+    # Raised when the values returned weren't the same as what was asked for
+    class InvalidValuesError < BaseError
+    end
 end
